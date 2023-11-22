633830ff2e0ef6f0b3e25d13fde059c3fac15b05
Matthias Baitsch <matthias.baitsch@me.com>
2023-11-22 06:58:45 +0100

    Change meaning of nx,ny from nodes to elements

--- src/utilities/generatemeshes.jl
+++ src/utilities/generatemeshes.jl
@@ -1,24 +1,24 @@
 """
     makemeshonrectangle(w::Number, h::Number, nx::Int, ny::Int)
 
-Generate a quad mesh on the domain ``[0, w] x [0, h]`` with `nx` nodes in the 
-``x`` direction and `ny` nodes in the ``y`` direction.
+Generate a quad mesh on the domain ``[0, w] \\times [0, h]`` with `nx` elements in the 
+``x`` direction and `ny` elements in the ``y`` direction.
 """
 function makemeshonrectangle(w::Number, h::Number, nx::Int, ny::Int)
 
     # Coordinates
-    nn = nx * ny
+    nn = (nx + 1) * (ny + 1)
     c = zeros(2, nn)
-    c[1, :] = repeat(range(0, stop=w, length=nx), outer=(ny, 1))
-    c[2, :] = repeat(range(0, stop=h, length=ny), inner=(nx, 1))
+    c[1, :] = repeat(range(0, stop=w, length=nx + 1), outer=(ny + 1, 1))
+    c[2, :] = repeat(range(0, stop=h, length=ny + 1), inner=(nx + 1, 1))
 
     # Mesh
     m = Mesh(c)
 
     # Connectivity
-    cl = ConnectivityList();
-    idx(i, j) = 1 + (j - 1) * nx + i
-    for i in 1:nx-1, j in 1:ny-1
+    cl = ConnectivityList()
+    idx(i, j) = (j - 1) * (nx + 1) + i
+    for j in 1:ny, i in 1:nx
         push!(cl, [idx(i, j), idx(i + 1, j), idx(i + 1, j + 1), idx(i, j + 1)])
     end
     addlinks!(m.topology, 2, 0, cl)
