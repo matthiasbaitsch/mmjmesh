4b9cc25cc4dbd2a2b3f58224b720edeb45764693
Matthias Baitsch <matthias.baitsch@me.com>
2024-01-15 17:15:15 +0100

    Refactor SeqIntSet

--- src/mmjbase/seqintset.jl
+++ src/mmjbase/seqintset.jl
@@ -4,111 +4,125 @@ SeqIntSet(a::AbstractVector{Int}; sorted=false)
 Set of integers which handles sets containing sequences, for example ```{-100, -99, -98, … , 2, 3, 9, 10, 12, … , 98, 100}```, efficiently.
 """
 struct SeqIntSet <: AbstractVector{Int}
-    count::Vector{Int}
-    sequences::Vector{Pair{Int,Int}}
+    start::Vector{Int}
+    step::Vector{Int}
+    index::Vector{Int}
 end
 
-function SeqIntSet(a::AbstractVector{Int}; sorted=false)
-    isempty(a) && return SeqIntSet([0], Vector{Pair{Int,Int}}())
-    asorted = sorted ? a : sort(a)
-    sequences = Vector{Pair{Int,Int}}()
-    start = stop = popfirst!(asorted)
-    for i ∈ eachindex(asorted)
-        if asorted[i] - stop <= 1
-            stop = asorted[i]
+# Constructor
+function SeqIntSet(a::AbstractVector{Int})
+
+    # Quick return
+    isempty(a) && return SeqIntSet(Int[], Int[], [1])
+
+    # Initialize
+    start = Int[]
+    step = Int[]
+    index = Int[]
+    b = unique(sort(a))
+    state = 0
+    step1 = step2 = NaN
+
+    # Read sequence
+    for i ∈ 2:length(b)
+
+        # Calculate steps
+        step1 = step2
+        step2 = b[i] - b[i-1]
+
+        # Process states
+        if state == 0
+            push!(index, i - 1)
+            push!(start, b[i-1])
+            state = 1
+        elseif state == 1
+            if step1 != step2
+                push!(step, step1)
+                push!(index, i - 1)
+                push!(start, b[i-1])
+            else
+                state = 2
+            end
         else
-            push!(sequences, Pair(start, stop))
-            start = stop = asorted[i]
+            if step1 != step2
+                push!(step, step1)
+                state = 0
+            end
         end
     end
-    push!(sequences, Pair(start, stop))
-    count = [p.second - p.first + 1 for p ∈ sequences] |> cumsum
-    return SeqIntSet(count, sequences)
-end
 
-function sequenceofvalue(target::Int, set::SeqIntSet)
-    low = 1
-    high = length(set.sequences)
-    while low <= high
-        mid = low + div(high - low, 2)
-        seq = set.sequences[mid]
-        if seq.first <= target && target <= seq.second
-            return mid
-        elseif seq.second < target
-            low = mid + 1
-        else
-            high = mid - 1
+    # Wrap up
+    if state <= 1
+        if state == 1
+            push!(step, step2)
         end
-    end
-    return -1
-end
-
-struct Seq
-    first::Int
-    inc::Int
-    last::Int
-    Seq(first::Int, inc::Int, last::Int) = new(first, inc, last - (last - first) % inc)
-end
-
-Base.first(s::Seq) = s.first
-Base.last(s::Seq) = s.last
-Base.in(i::Int, s::Seq) = s.first <= i <= s.last && (i - s.first) % s.inc == 0
-
-function Base.show(io::IO, s::Seq)
-    if s.first == s.last
-        print(io, s.first)
-    elseif s.inc == 1
-        print(io, s.first, ":", s.last)
+        push!(index, length(b))
+        push!(start, b[end])
+        push!(step, 0)
     else
-        print(io, s.first, ":", s.inc, ":", s.last)
+        push!(step, step1)
     end
+    push!(index, length(b) + 1)
+
+    # Return
+    return SeqIntSet(start, step, index)
 end
 
+# Helpers
+nsequences(set::SeqIntSet) = length(set.start)
+nsteps(set::SeqIntSet, s::Int) = set.index[s+1] - set.index[s] - 1
+start(set::SeqIntSet, s::Int) = set.start[s]
+step(set::SeqIntSet, s::Int) = set.step[s]
+stop(set::SeqIntSet, s::Int) = set.start[s] + nsteps(set, s) * set.step[s]
+index(set::SeqIntSet, s::Int) = set.index[s]
 
-nsequences(set::SeqIntSet) = length(set.sequences)
-function sequence(set::SeqIntSet, i::Int) 
-    s = set.sequences[i]
-    return Seq(s.first, 1, s.second)
+# Base
+Base.first(set::SeqIntSet) = first(set.start)
+Base.length(set::SeqIntSet) = set.index[end] - 1
+Base.size(set::SeqIntSet) = (length(set),)
+Base.isempty(set::SeqIntSet) = (length(set) == 0)
+Base.last(set::SeqIntSet) = set.start[end] + nsteps(set, nsequences(set)) * set.step[end]
+Base.union(s1::SeqIntSet, s2::SeqIntSet) = SeqIntSet([collect(s1); collect(s2)])
+Base.intersect(s1::SeqIntSet, s2::SeqIntSet) = SeqIntSet([i for i in s1 if i ∈ s2])
+Base.setdiff(s1::SeqIntSet, s2::SeqIntSet) = SeqIntSet([i for i in s1 if i ∉ s2])
+
+# Getindex
+function Base.getindex(set::SeqIntSet, i::Int)
+    @assert 1 <= i <= length(set)
+    idx = searchsortedlast(set.index, i)
+    return set.start[idx] + (i - set.index[idx]) * set.step[idx]
 end
 
-function sequences(s::SeqIntSet)
-    return [Seq(p.first, 1, p.second) for p ∈ s.sequences]
+# In
+function Base.in(target::Int, set::SeqIntSet)
+    s = searchsortedlast(set.start, target)
+    s == 0 && return false
+    target > stop(set, s) && return false
+    step(set, s) == 0 && return true
+    return (target - start(set, s)) % step(set, s) == 0
 end
 
-Base.first(set::SeqIntSet) = first(first(set.sequences))
-Base.length(s::SeqIntSet) = s.count[end]
-Base.size(s::SeqIntSet) = (length(s),)
-Base.isempty(s::SeqIntSet) = (length(s) == 0)
-Base.in(target::Int, set::SeqIntSet) = sequenceofvalue(target, set) != -1
-Base.getindex(s::SeqIntSet, i::Int) = collect(s)[i]
-
-function Base.show(io::IO, ::MIME{Symbol("text/plain")}, set::SeqIntSet) 
-    print(io, "SeqIntSet(ns=$(nsequences(set)), ne=$(length(set)))\n")
-    print(io, join(sequences(set), "\n"))
-end
-Base.show(io::IO, set::SeqIntSet) = print(io, "[$(join(sequences(set), ", "))]")
-
-# TODO: direct access
-Base.first(set::SeqIntSet, idx::Int) = first(sequence(set, idx))
-inc(set::SeqIntSet, idx::Int) = sequence(set, idx).inc
-Base.last(set::SeqIntSet, idx::Int) = last(sequence(set, idx))
-Base.last(set::SeqIntSet) = last(set.sequences[end])
-
-# Iterator using the pair (position, value) as state
+# Iterate
 Base.eltype(::SeqIntSet) = Int
-Base.iterate(set::SeqIntSet) = isempty(set) ? nothing : (first(set), (1, first(set)))
+Base.iterate(set::SeqIntSet) = isempty(set) ? nothing : (first(set), (1, 2))
 function Base.iterate(set::SeqIntSet, state)
-    pos, val = state
-    if pos <= nsequences(set) && val < last(set)
-        if val == last(set, pos)
-            pos += inc(set, pos)
-            val = first(set, pos)
-        else
-            val += inc(set, pos)
+    sequence, index = state
+    if sequence <= nsequences(set) && index <= length(set)
+        if index == set.index[sequence+1]
+            sequence += 1
         end
-        return (val, (pos, val))
-    else
-        return nothing
+        offset = index - set.index[sequence]
+        value = set.start[sequence] + offset * set.step[sequence]
+        return (value, (sequence, index + 1))
     end
+    return nothing
 end
 
+# Show
+_ts(start, step, stop) = start == stop ? string(start) : step == 1 ? string(start, ":", stop) : string(start, ":", step, ":", stop)
+_tsseq(set::SeqIntSet) = [_ts(start(set, i), step(set, i), stop(set, i)) for i in 1:nsequences(set)]
+function Base.show(io::IO, ::MIME{Symbol("text/plain")}, set::SeqIntSet)
+    print(io, "SeqIntSet(ns=$(nsequences(set)), ne=$(length(set)))\n")
+    print(io, join(_tsseq(set), "\n"))
+end
+Base.show(io::IO, set::SeqIntSet) = print(io, "[$(join(_tsseq(set), ", "))]")
--- test/mmjbase/seqintsettests.jl
+++ test/mmjbase/seqintsettests.jl
@@ -1,8 +1,52 @@
 using Test
 
 using MMJMesh
-import MMJMesh.MMJBase: SeqIntSet, Seq, sequenceofvalue, nsequences, sequence, sequences
+import MMJMesh.MMJBase: SeqIntSet, nsequences, start, step, nsteps, stop
 
+
+# -------------------------------------------------------------------------------------------------
+# Constructor
+# -------------------------------------------------------------------------------------------------
+
+# Three sequences
+a = [1, 3, 5, 6, 7, 8, 9, 11, 14, 17]
+set = SeqIntSet(a);
+@test set.index == [1, 4, 8, 11]
+@test set.start == [1, 6, 11]
+@test set.step == [2, 1, 3]
+
+# No sequences
+a = [1, 2, 4, 5, 7]
+set = SeqIntSet(a);
+@test set.start == a
+@test set.step == [1, 2, 1, 2, 0]
+@test set.index == collect(1:(length(a)+1))
+
+# One sequence
+a = [3, 5, 7, 9, 11]
+set = SeqIntSet(a);
+@test set.start == [3]
+@test set.step == [2]
+@test set.index == [1, 6]
+
+# One sequence, one single number at the end
+a = [3, 5, 7, 9, 11, 100]
+set = SeqIntSet(a);
+@test set.start == [3, 100]
+@test set.step == [2, 0]
+@test set.index == [1, 6, 7]
+
+# Two sequences, three single numbers
+a = [0, 2, 3, 4, 6, 8, 10, 30, 31]
+set = SeqIntSet(a);
+@test set.index == [1, 2, 5, 8, 9, 10]
+@test set.start == [0, 2, 6, 30, 31]
+@test set.step == [2, 1, 2, 1, 0]
+
+
+# -------------------------------------------------------------------------------------------------
+# Methods
+# -------------------------------------------------------------------------------------------------
 
 function validate(a::AbstractVector, set::SeqIntSet)
     b = unique(sort(a))
@@ -19,68 +63,81 @@ function validate(a::AbstractVector, set::SeqIntSet)
     for i ∈ eachindex(b)
         @test set[i] == b[i]
     end
+    @test set == b
+    @test collect(set) == b
 end
 
 
-# Seq
-s = Seq(-3, 2, 10)
-@test s.last == 9
-@test -4 ∉ s
-@test -3 ∈ s
-@test -1 ∈ s
-@test 9 ∈ s
-@test 10 ∉ s
-@test 11 ∉ s
+# Methods basic tests
+a = [0, 2, 3, 4, 6, 8, 10, 30, 31]
+set = SeqIntSet(a)
+
+@test nsequences(set) == 5
+@test first(set) == 0
+@test length(set) == 9
+@test size(set) == (9,)
+@test !isempty(set)
+@test last(set) == 31
+@test start(set, 1) == 0
+@test step(set, 1) == 2
+@test nsteps(set, 1) == 0
+@test stop(set, 1) == 0
+
+
+# Empty
+set = SeqIntSet(Vector{Int}());
+@test isempty(set)
+@test collect(set) == Vector{Int}()
+@test 5 ∉ set
+
+
+# One element
+set = SeqIntSet([1])
+@test length(set) == 1
+@test collect(set) == [1]
+@test 1 ∈ set
+@test -1 ∉ set
+@test 2 ∉ set
+validate([1], set)
+
 
 # Many elements
 a = [1, 10, -50, 2, 11, 9, 8, 0, 100]
-s = SeqIntSet(a);
+set = SeqIntSet(a);
 
-@test nsequences(s) == 4
-@test size(s) == (9,)
-@test length(s) == 9
-@test collect(s) == [-50, 0, 1, 2, 8, 9, 10, 11, 100]
-@test -1000 ∉ s
-@test -51 ∉ s
-@test 5 ∉ s
-@test 12 ∉ s
-@test 101 ∉ s
-@test 1000 ∉ s
-validate(a, s)
-
-# One element
-s = SeqIntSet([1])
-@test length(s) == 1
-@test collect(s) == [1]
-@test 1 ∈ s
-@test -1 ∉ s
-@test 2 ∉ s
-
-# Empty
-s = SeqIntSet(Vector{Int}())
-@test isempty(s)
-@test collect(s) == Vector{Int}()
-@test 5 ∉ s
-
-# Array indexing
-a = collect(1:10)
-s = SeqIntSet([1, 2, 3, 5, 9, 10])
-@test a[s] == s
-
-# TODO: Set operations
-# s1 = SeqIntSet([1, 2, 3, 5, 9, 10])
-# s2 = SeqIntSet([1, 2, 3, 4, 5, 6])
-
-# Base.union(s1::SeqIntSet, s2::SeqIntSet) = SeqIntSet(union(s1.sequences, s2.sequences))
-
-# @test s1 ∪ s2 == [1, 2, 3, 4, 5, 6, 9, 10]
-# @test s1 ∩ s2 == [1, 2, 3, 5]
+@test nsequences(set) == 4
+@test size(set) == (9,)
+@test length(set) == 9
+@test collect(set) == [-50, 0, 1, 2, 8, 9, 10, 11, 100]
+@test -1000 ∉ set
+@test -51 ∉ set
+@test 1 ∈ set
+@test 5 ∉ set
+@test 12 ∉ set
+@test 100 ∈ set
+@test 101 ∉ set
+@test 1000 ∉ set
+@test set[1] == -50
+@test set[2] == 0
+@test set[3] == 1
+@test set[4] == 2
+@test set[5] == 8
+@test set[8] == 11
+@test set[9] == 100
+validate(a, set)
 
 
-
-# Test case from a mesh
+# Test case from an actual mesh
 a1 = [1, 2, 1, 12, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11, 22, 12, 23, 22, 33, 23, 34, 33, 44, 34, 45, 44, 55, 45, 56, 55, 66, 56, 67, 66, 77, 67, 78, 77, 88, 78, 89, 88, 99, 89, 100, 99, 110, 100, 111, 110, 121, 111, 122, 121, 132, 122, 133, 132, 143, 133, 144, 143, 154, 144, 155, 154, 165, 166, 167, 155, 166, 167, 168, 168, 169, 169, 170, 170, 171, 171, 172, 172, 173, 173, 174, 174, 175, 165, 176, 175, 176]
-a2 = unique(a1)
 
-@test SeqIntSet(a1) == SeqIntSet(a2)
 validate(a1, SeqIntSet(a1))
+@test SeqIntSet(a1) == SeqIntSet(unique(a1))
+
+
+# Set operations
+s1 = SeqIntSet([1, 2, 3, 5, 9, 10])
+s2 = SeqIntSet([1, 2, 3, 4, 5, 6])
+
+@test s1 ∪ s2 == [1, 2, 3, 4, 5, 6, 9, 10]
+@test s1 ∩ s2 == [1, 2, 3, 5]
+@test setdiff(s1, s2) == [9, 10]
