9d788de74921bcaeb07a9ceeb92dca26a469def4
Matthias Baitsch <matthias.baitsch@me.com>
2024-05-14 14:11:11 +0200

    Iterator for PointList

--- src/geometries/detail.jl
+++ src/geometries/detail.jl
@@ -2,3 +2,7 @@ mutable struct PointList
     n::Int
     coordinates::Matrix{Float64}
 end
+
+Base.iterate(pl::Geometries.PointList) = (pl.coordinates[:, 1], 2)
+Base.iterate(pl::Geometries.PointList, i::Int) = i <= pl.n ? (pl.coordinates[:, i], i + 1) : nothing
+Base.length(pl::PointList) = pl.n
