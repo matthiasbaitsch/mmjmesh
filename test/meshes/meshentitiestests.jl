3f0a7555673bb70e65ef8fad5ed3d968a87507ed
Matthias Baitsch <matthias.baitsch@me.com>
2024-07-01 21:12:27 +0200

    Test pdim and gdim

--- test/meshes/meshentitiestests.jl
+++ test/meshes/meshentitiestests.jl
@@ -3,6 +3,8 @@ using Test
 using MMJMesh.Meshes
 using MMJMesh.Geometries
 
+using MMJMesh.Meshes: pdim, gdim
+
 
 # Set up
 coords = [0.0 1.0 2.0 0.1 0.9 1.9; 0.0 0.1 0.0 0.9 1.0 0.9]
@@ -15,6 +17,8 @@ m = Mesh(coords, elts, 2)
 # -------------------------------------------------------------------------------------------------
 
 n5 = node(m, 5)
+@test pdim(n5) == 0
+@test gdim(n5) == 2
 @test nedges(n5) == 3
 @test coordinates(n5) == [0.9, 1.0]
 @test coordinate(n5, 1) == 0.9
@@ -26,6 +30,8 @@ n5 = node(m, 5)
 # -------------------------------------------------------------------------------------------------
 
 e2 = edge(m, 2)
+@test pdim(e2) == 1
+@test gdim(e2) == 2
 @test length(e2) == hypot(0.1, 0.9)
 
 g = geometry(e2)
@@ -38,6 +44,8 @@ p = parametrization(g)
 # -------------------------------------------------------------------------------------------------
 
 f1 = face(m, 1)
+@test pdim(f1) == 2
+@test gdim(f1) == 2
 @test nnodes(f1) == 4
 @test nodeindices(f1) == [1, 2, 5, 4]
 @test edgeindices(f1) == [1, 2, 3, 4]
@@ -50,7 +58,7 @@ f1 = face(m, 1)
 f3 = face(m, 3)
 @test nnodes(f3) == 3
 @test nodeindices(f3) == [2, 6, 5]
-@test coordinates(f3) == [1.0  1.9  0.9; 0.1 0.9 1.0]
+@test coordinates(f3) == [1.0 1.9 0.9; 0.1 0.9 1.0]
 @test coordinates(f3, 1) == [1.0, 0.1]
 @test coordinates(f3, 2) == [1.9, 0.9]
 @test coordinates(f3, 3) == [0.9, 1.0]
