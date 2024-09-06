c4ef39d9df310c9aa2f03cc52465302dab0033de
Matthias Baitsch <matthias.baitsch@me.com>
2024-09-06 07:36:12 +0200

    .

--- doc/apidoc/make.jl
+++ doc/apidoc/make.jl
@@ -20,10 +20,3 @@ makedocs(
         "Mathematics" => "mathematics.md"
     ]
 )
-
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-deploydocs(
-    repo = "<repository url>"
-)
