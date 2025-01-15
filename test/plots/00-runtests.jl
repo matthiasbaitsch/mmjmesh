0c7c3272e7a89a81f51dd59657cb334885b2ee53
Matthias Baitsch <matthias.baitsch@me.com>
2025-01-15 17:28:12 +0100

    Remove obsolete tests

--- test/plots/00-runtests.jl
+++ test/plots/00-runtests.jl
@@ -8,26 +8,6 @@ using MMJMesh.Plots
 using MMJMesh.Meshes
 using MMJMesh.Utilities
 
-@testset "Basic" begin
-
-    # 1D, horizontal
-    m = makemeshoninterval(0.0, 1.2, 10)
-    mplot(m, -1.1 .+ 2.2 * rand(nnodes(m)))
-    @test true
-
-    # 1D, vertical
-    m = makemeshoninterval(π, 3π, 20, t -> [0; t])
-    mplot(m, -1.1 .+ 3.2 * rand(nedges(m)))
-    @test true
-
-    # 2D
-    a = 3
-    m = makemeshonrectangle(9.0, 4.5, 2a, a)
-    mplot(m, 4.1 * (rand(nnodes(m)) .- 0.25))
-    @test true
-
-end
-
 @testset "Helperfunctions" include("helperfunctionstests.jl")
 @testset "Sample1D" include("sample1dtests.jl")
 @testset "CurveApproximation" include("curveapproximationtests.jl")
