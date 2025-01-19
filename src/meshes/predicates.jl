3c64053e7f9cbbde5995109e59ddc5f17a297952
Matthias Baitsch <matthias.baitsch@me.com>
2025-01-19 11:01:30 +0100

    .

--- src/meshes/predicates.jl
+++ src/meshes/predicates.jl
@@ -1,5 +1,13 @@
 using MMJMesh
 using MMJMesh.Geometries
 
+
+"""
+    on(p::Point)
+    on(s::GeometricObjectP)
+
+Generates a predicate which tests if a node is on the specified geometric object. To be used
+in conjunction with the `nodes(m, predicate)` function.
+"""
 on(s::GeometricObjectP; atol=1e-12) = n::Node -> parameterof(s, coordinates(n), atol=atol)
 on(p::Point; atol=1e-12) = n -> isapprox(p.coordinates, coordinates(n), atol=atol) ? 1 : NaN
