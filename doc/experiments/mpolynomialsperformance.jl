e1b916761c6d6cda2527eeea77fdc48b6b0a888c
Matthias Baitsch <matthias.baitsch@me.com>
2025-04-15 07:39:59 +0200

    Add MPolynomial performance test

--- doc/experiments/mpolynomialsperformance.jl
+++ doc/experiments/mpolynomialsperformance.jl
@@ -3,6 +3,11 @@ using BenchmarkTools
 
 using MMJMesh.Mathematics
 
+
+# -------------------------------------------------------------------------------------------------
+# Monomial evaluation
+# -------------------------------------------------------------------------------------------------
+
 function monomialsat1(exponents::SMatrix{N,NT}, x::InRⁿ{N}) where {N,NT}
     c = MVector{NT,Float64}(undef)
     for i = 1:NT
@@ -36,7 +41,30 @@ end
 
 e = SA[1 2 3 4 5; 4 3 3 2 1; 1 2 1 2 1]
 x = SA[1.2, 2.1, 1.1]
+
 @btime monomialsat1(e, x);
 @btime monomialsat2(e, x);
 @btime monomialsat3(e, x);
 
+
+
+# -------------------------------------------------------------------------------------------------
+# MPolynomial evaluation
+# -------------------------------------------------------------------------------------------------
+
+# Evaluate monomials
+m = _monomialsat(e, x);
+println("Evaluation of monomials")
+@btime _monomialsat(e, x);
+
+# Scalar valued 
+c1 = SA[4, 3, 2, 2, 9]
+p1 = MPolynomial(e, c1);
+@btime m ⋅ c1;
+@btime valueat(p1, x);
+
+# Vector valued 
+c2 = SA[4 3 2 2 9; 5 4 5 4 3; 7 7 1 2 9];
+p2 = MPolynomial(e, c2);
+@btime c2 * m;
+@btime valueat(p2, x);
