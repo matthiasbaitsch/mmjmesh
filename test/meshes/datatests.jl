6a38c77da34c9c04a021c46d291a8c5038130310
Matthias Baitsch <matthias.baitsch@me.com>
2025-04-21 09:19:29 +0200

    Test for reassigning data

--- test/meshes/datatests.jl
+++ test/meshes/datatests.jl
@@ -1,10 +1,12 @@
 using Test
 using MMJMesh.Meshes
 
-# Create test mesh
-coords = [0.0 1.0 2.0 0.1 0.9 1.9; 0.0 0.1 0.0 0.9 1.0 0.9]
-elts = [[1, 2, 5, 4], [2, 3, 6], [2, 6, 5]]
-m = Mesh(coords, elts, 2)
+# -------------------------------------------------------------------------------------------------
+# Basic functionality
+# -------------------------------------------------------------------------------------------------
+
+# Test mesh
+m = Mesh(:quadtri)
 definegroup!(m, 2, :bar, [2])
 
 # Assign data
@@ -14,7 +16,7 @@ setdata!(group(m, :elements), :foo, 3)
 setdata!(node(m, 1), :foo, 4)
 setdata!(element(m, 1), :foo, 5)
 
-# Test direct access
+# Test data(...)
 @test data(m, :foo) == 1
 @test data(group(m, :bar), :foo) == 2
 @test data(group(m, :elements), :foo) == 3
@@ -25,17 +27,43 @@ setdata!(element(m, 1), :foo, 5)
 @test data(edge(m, 1), :foo) == 1
 @test data(edge(m, 1), :baz) === nothing
 
+# Test hasdata(...)
 @test hasdata(edge(m, 1), :foo)
 @test !hasdata(edge(m, 1), :baz)
 
-# Test array access
-coords = [0.0 1.0 2.0 0.1 0.9 1.9; 0.0 0.1 0.0 0.9 1.0 0.9]
-elts = [[1, 2, 5, 4], [2, 3, 6], [2, 6, 5]]
-m = Mesh(coords, elts, 2)
+# Test reassign data
+setdata!(m, :foo, 2)
+setdata!(group(m, :bar), :foo, 3)
+setdata!(group(m, :elements), :foo, 4)
+setdata!(node(m, 1), :foo, 5)
+setdata!(element(m, 1), :foo, 6)
+@test data(m, :foo) == 2
+@test data(group(m, :bar), :foo) == 3
+@test data(group(m, :elements), :foo) == 4
+@test data(node(m, 1), :foo) == 5
+@test data(element(m, 1), :foo) == 6
+@test data(element(m, 2), :foo) == 3
+@test data(element(m, 3), :foo) == 4
+@test data(edge(m, 1), :foo) == 2
+@test data(edge(m, 1), :baz) === nothing
+
+## XXX
+
+# -------------------------------------------------------------------------------------------------
+# Test array data
+# -------------------------------------------------------------------------------------------------
+
+# Test mesh
+m = Mesh(:quadtri)
+
+# Assign data
 setdata!(m, :foo, 99)
 setdata!(m, :bar, [4, 1, 8])
 
+# Test data(...)
 @test data(element(m, 1), :foo) == 99
 @test data(element(m, 1), :bar) == 4
 @test data(element(m, 2), :bar) == 1
 @test data(element(m, 3), :bar) == 8
+
+
