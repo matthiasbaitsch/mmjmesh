b51a2fe90d86a00d1802aa3130edacbeac192926
Matthias Baitsch <matthias.baitsch@me.com>
2025-10-15 23:07:26 +0200

    .

--- src/plots/vplot.jl
+++ src/plots/vplot.jl
@@ -31,9 +31,11 @@ function MakieCore.plot!(plot::VPlot)
         if color == :norm
             color = norm.(u)
         end
-        MakieCore.arrows2d!(
-            plot, MakieCore.Point2f.(p), MakieCore.Vec2f.(u), linecolor=color,
-            arrowcolor=color, arrowsize=as, lengthscale=ls, colormap=cm
+        Makie.arrows2d!(
+            plot, MakieCore.Point2f.(p), MakieCore.Vec2f.(u), 
+            # FIXME
+            # linecolor=color,
+            # arrowcolor=color, arrowsize=as, lengthscale=ls, colormap=cm
         )
     end
     return plot
