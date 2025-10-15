7946c4360c54d08ccba9c4cf0c76b6d2a5947bf6
Matthias Baitsch <matthias.baitsch@me.com>
2025-10-15 22:52:18 +0200

    Use arrows2d

--- src/plots/feplot.jl
+++ src/plots/feplot.jl
@@ -61,9 +61,11 @@ function MakieCore.plot!(plot::FEPlot)
     end
 
     if !isempty(pMixedDerivativeAt)
-        MakieCore.arrows!(
-            plot, pMixedDerivativeAt,
-            repeat([1, 1], length(pMixedDerivativeAt)), lengthscale=0.2, arrowsize=10
+        MakieCore.arrows2d!(
+            plot, 
+            pMixedDerivativeAt,
+            repeat([MakieCore.Vec2f(1, 1)], length(pMixedDerivativeAt)), 
+            lengthscale=0.2
         )
     end
 
